d92b108ebc2ec95c9a3b5b041511c8e245ec15af
mcrd25 <mayacdouglas@gmail.com>
2023-08-23 04:28:54 -0400

    test dataset creation

--- main.py
+++ main.py
@@ -12,5 +12,27 @@ torch.manual_seed(0)
 
 class_names = ['normal', 'viral', 'covid']
 root_dir = 'dataset'
-source_dirs = ['NORMAL', 'Viral Pneumonia', 'COVID-19']
+source_dirs = ['normal', 'viral', 'covid']
 
+
+# code to create test directory, not needed if you already have it
+
+"""
+if os.path.isdir(os.path.join(root_dir, source_dirs[1])):
+    os.mkdir(os.path.join(root_dir, 'test'))
+
+    for i, d in enumerate(source_dirs):
+        os.rename(os.path.join(root_dir, d), os.path.join(root_dir, class_names[i]))
+
+    for c in class_names:
+        os.mkdir(os.path.join(root_dir, 'test', c))
+
+    for c in class_names:
+        images = [x for x in os.listdir(os.path.join(root_dir, c)) if x.lower().endswith('png')]
+        selected_images = random.sample(images, 30)
+        for image in selected_images:
+            source_path = os.path.join(root_dir, c, image)
+            target_path = os.path.join(root_dir, 'test', c, image)
+            shutil.move(source_path, target_path)
+
+"""
